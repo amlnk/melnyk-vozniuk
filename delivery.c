842af6d8040ef0912ab64280aa6e9db34f4e80c8
Alina Vozniuk <alinavoznuk2007@gmail.com>
2025-07-17 00:49:39 +0300

    Implemented delivery area selection via inputDeliveryAddress

--- delivery.c
+++ delivery.c
@@ -11,3 +11,33 @@ void showDeliveryOptions() {
     printf("5. Darnytskyi\n");
     printf("0. Cancel\n");
 }
+void inputDeliveryAddress() {
+    int choice;
+    showDeliveryOptions();
+    printf("Choose delivery district: ");
+    scanf("%d", &choice);
+    getchar();
+
+    switch (choice) {
+        case 1:
+            printf("Delivery selected: Shevchenkivskyi district\n");
+            break;
+        case 2:
+            printf("Delivery selected: Pecherskyi district\n");
+            break;
+        case 3:
+            printf("Delivery selected: Solomianskyi district\n");
+            break;
+        case 4:
+            printf("Delivery selected: Holosiivskyi district\n");
+            break;
+        case 5:
+            printf("Delivery selected: Darnytskyi district\n");
+            break;
+        case 0:
+            printf("Delivery cancelled.\n");
+            break;
+        default:
+            printf("Invalid choice.\n");
+    }
+}
