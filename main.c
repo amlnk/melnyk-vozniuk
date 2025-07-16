22d56c9cb169b6d717bc9c79d55366063d0fcc89
Аліна Мельник <kn24-a.melnyk@nubipl.edu.ua>
2025-07-17 01:29:20 +0300

    Main.c is finalized — main control is implemented- Modules are connected: registration, menu, cart, delivery - Main menu with action processing is added - Launch of login, registration, food selection and delivery-Melnyk and Vozniuk

--- main.c
+++ main.c
@@ -1,24 +1,56 @@
 #include <stdio.h>
-#include "menu.h"
 #include "cart.h"
+#include "menu.h"
+#include "registration.h"
+#include "delivery.h"
 
 int main() {
+    UserList users;
+    initUsers(&users);
+
     Cart cart;
     initCart(&cart);
 
-    int choice;
+    int loggedIn = 0;
+    int mainChoice, authChoice;
 
+    
+    while (!loggedIn) {
+        printf("\n=== Welcome to Food Order Service ===\n");
+        printf("1. Register\n");
+        printf("2. Login\n");
+        printf("0. Exit\n");
+        printf("Choose an option: ");
+        scanf("%d", &authChoice);
+        getchar();
+
+        switch (authChoice) {
+            case 1:
+                registerUser(&users);
+                break;
+            case 2:
+                loggedIn = loginUser(&users);
+                break;
+            case 0:
+                printf("Goodbye!\n");
+                return 0;
+            default:
+                printf("Invalid choice.\n");
+        }
+    }
+
+   
     while (1) {
         printf("\n=== FOOD ORDER SERVICE ===\n");
         printf("1. Add dish to cart\n");
         printf("2. View cart\n");
         printf("3. Checkout\n");
         printf("0. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-        getchar(); 
+        printf("Choose an option: ");
+        scanf("%d", &mainChoice);
+        getchar();
 
-        switch (choice) {
+        switch (mainChoice) {
             case 1:
                 addToCart(&cart);
                 break;
@@ -27,15 +59,15 @@ int main() {
                 break;
             case 3:
                 checkout(&cart);
+                inputDeliveryAddress();
                 break;
             case 0:
-                printf("Thank you! Goodbye!\n");
+                printf("Goodbye!\n");
                 return 0;
             default:
-                printf("Invalid choice. Try again.\n");
+                printf("Invalid choice.\n");
         }
     }
 
     return 0;
 }
-
