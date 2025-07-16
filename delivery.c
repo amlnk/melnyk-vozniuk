de5c639cbceeb2d8710c898fe607ed1816d5bf5c
Alina Vozniuk <alinavoznuk2007@gmail.com>
2025-07-17 00:46:46 +0300

    A framework for selecting a delivery address has been created.

---
+++ delivery.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include "delivery.h"
+
+void showDeliveryOptions() {
+    printf("\n=== Delivery Areas ===\n");
+    printf("1. Shevchenkivskyi\n");
+    printf("2. Pecherskyi\n");
+    printf("3. Solomianskyi\n");
+    printf("4. Holosiivskyi\n");
+    printf("5. Darnytskyi\n");
+    printf("0. Cancel\n");
+}
